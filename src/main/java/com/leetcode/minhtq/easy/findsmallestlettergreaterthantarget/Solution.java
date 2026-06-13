c604745f32316b92ed68583981f1fa251a6887c8
Tạ Quang Minh <quangminh8699@gmail.com>
2026-06-13 15:04:27 +0700

    Find Smallest Letter Greater Than Target

--- src/main/java/com/leetcode/minhtq/easy/findsmallestlettergreaterthantarget/Solution.java
+++ src/main/java/com/leetcode/minhtq/easy/findsmallestlettergreaterthantarget/Solution.java
@@ -2,7 +2,25 @@ package com.leetcode.minhtq.easy.findsmallestlettergreaterthantarget;
 
 public class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        return 'a';
+        int l = 0;
+        int r = letters.length - 1;
+
+        while (l <= r) {
+            int m = (l + r) / 2;
+            if (letters[m] > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return letters[l % letters.length];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        char[] letters = {'x','x','y','y'};
+        System.out.println(solution.nextGreatestLetter(letters, 'z'));
     }
 }
 

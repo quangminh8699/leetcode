029b697f5ccb5ab96a439463407e736464899b9e
Tạ Quang Minh <quangminh8699@gmail.com>
2026-06-22 10:54:11 +0700

    single number

---
+++ src/main/java/com/leetcode/minhtq/easy/singlenumber/Solution.java
@@ -0,0 +1,11 @@
+package com.leetcode.minhtq.easy.singlenumber;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
